b7ec2c7d8b8e1b619ac78bb393cdcab791afa852
Prateek Arora <official.aroraprateek@gmail.com>
2021-01-21 20:22:13 +0530

    Added feature to create new blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_new
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>', post_detail, name='post_detail'),
+    path('post/new', post_new, name='post_new'),
 
 ]
 
