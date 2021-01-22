0b67ad107942deae2bc2add864048fb5e5abc556
Prateek Arora <official.aroraprateek@gmail.com>
2021-01-22 17:59:15 +0530

    Added a feature to save drafts and then publish them later.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from blog.views import post_list, post_detail, post_new, post_edit
+from blog.views import post_list, post_detail, post_new, post_edit, post_draft_list, post_publish
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>', post_detail, name='post_detail'),
     path('post/new', post_new, name='post_new'),
     path('post/<int:pk>/edit', post_edit, name='post_edit'),
+    path('drafts/', post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', post_publish, name='post_publish'),
 
 ]
 
