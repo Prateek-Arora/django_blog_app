aa97593ef3e6085c2482aefe88951e97bafb178d
Prateek Arora <official.aroraprateek@gmail.com>
2021-01-28 23:18:21 +0530

    Require login to add, remove or approve a comment.

--- blog/views.py
+++ blog/views.py
@@ -16,13 +16,16 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
+        if request.user.is_authenticated:
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.author = request.user
+                comment.post = post
+                comment.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            return redirect('login')
     else:
         form = CommentForm()
     stuff_for_frontend = {'post': post, 'form': form}
@@ -90,13 +93,14 @@ def post_publish(request, pk):
 #         form = CommentForm()
 #     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
-
+@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
 
+@login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
