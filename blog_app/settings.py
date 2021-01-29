1bb24a0652a9fc2ae8b17f75d593b6b49dd2fc81
Prateek Arora <official.aroraprateek@gmail.com>
2021-01-29 19:58:13 +0530

    Added markdown and syntax highlighting for blog posts and comments.

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -68,6 +68,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+                'mardownify': 'blog.templatetags.markdownify',
+
+            }
         },
     },
 ]
@@ -123,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
